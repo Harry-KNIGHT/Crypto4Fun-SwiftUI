0ebdf72e2342746bf8acf61b28c1683e3e544152
Nyl <95429032+NylNeuville@users.noreply.github.com>
2022-07-10 12:51:08 +0200

    userdefault NFT

--- CryptoNoob/ViewModel/FavoriteNftsViewModel.swift
+++ CryptoNoob/ViewModel/FavoriteNftsViewModel.swift
@@ -8,7 +8,24 @@
 import Foundation
 
 class FavoriteNftsViewModel: ObservableObject {
-    @Published var favoriteNfts = [NFTModel]()
+    @Published var favoriteNfts: [NFTModel]
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: "SavedData") {
+            if let decoded = try? JSONDecoder().decode([NFTModel].self, from: data) {
+                favoriteNfts = decoded
+                return
+            }
+        }
+        // No Saved data
+        favoriteNfts = []
+    }
+
+    func saveNft() {
+        if let encoded = try? JSONEncoder().encode(favoriteNfts) {
+            UserDefaults.standard.set(encoded, forKey: "SavedData")
+        }
+    }
     
     func addNft(nft: NFTModel) {
         favoriteNfts.append(nft)
@@ -24,12 +41,15 @@ class FavoriteNftsViewModel: ObservableObject {
         
         if favoriteNfts.contains(nft) {
             removeNft(nft: nft)
+            saveNft()
         } else {
             addNft(nft: nft)
+            saveNft()
         }
     }
     
     func deleteNft(nft: IndexSet) {
         favoriteNfts.remove(atOffsets: nft)
+        saveNft()
     }
 }
