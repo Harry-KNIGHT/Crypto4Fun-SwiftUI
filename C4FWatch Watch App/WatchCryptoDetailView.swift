a9051ab650ab1536530fc08d5c63299b6ffd757d
Harry-KNIGHT <eliott.knight@icloud.com>
2022-12-09 15:13:12 +0100

    Watch detail crypto view

---
+++ C4FWatch Watch App/WatchCryptoDetailView.swift
@@ -0,0 +1,43 @@
+//
+//  WatchCryptoDetailView.swift
+//  C4FWatch Watch App
+//
+//  Created by Elliot Knight on 09/12/2022.
+//
+
+import SwiftUI
+import Crypto4FunKit
+
+struct WatchCryptoDetailView: View {
+	let crypto: CryptoCurrencyModel
+	var body: some View {
+		VStack(alignment: .leading) {
+			HStack {
+				AsyncImageView(cryptoCurrency: crypto, width: 50, height: 50)
+
+				Spacer()
+				Text("\(String(format: "%.2f", crypto.priceChangePercentage24h))%")
+					.font(.callout)
+					.foregroundColor(crypto.priceChangePercentage24h > 0 ? .green : .red)
+
+			}
+			Spacer()
+			Text(crypto.name.capitalized)
+				.font(.title2)
+				.fontWeight(.medium)
+			Spacer()
+			Text("$\(crypto.currentPrice.description)")
+				.font(.title)
+				.foregroundColor(crypto.priceChangePercentage24h > 0 ? .green : .red	)
+
+			Spacer()
+		}
+		.fontDesign(.rounded)
+	}
+}
+
+struct WatchCryptoDetailView_Previews: PreviewProvider {
+	static var previews: some View {
+		WatchCryptoDetailView(crypto: .cryptoSample)
+	}
+}
