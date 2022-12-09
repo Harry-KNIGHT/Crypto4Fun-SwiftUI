19f8929eb43b012d42dc1dcf2320a2b66567f323
Harry-KNIGHT <eliott.knight@icloud.com>
2022-12-09 14:54:25 +0100

    Add Apple watch extension and list of crypto into watch view.

---
+++ C4FWatch Watch App/C4FWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  C4FWatchApp.swift
+//  C4FWatch Watch App
+//
+//  Created by Elliot Knight on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct C4FWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C4FWatch Watch App/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  C4FWatch Watch App
+//
+//  Created by Elliot Knight on 09/12/2022.
+//
+
+import SwiftUI
+import Crypto4FunKit
+
+struct ContentView: View {
+	@State private var cryptos = [CryptoCurrencyModel]()
+	var body: some View {
+		NavigationView {
+			VStack {
+				List {
+					ForEach(cryptos, id: \.id) { crypto in
+						VStack(alignment: .leading, spacing: 2) {
+							HStack(spacing: 0) {
+								Text(crypto.name)
+									.font(.caption)
+									.lineLimit(1)
+								Spacer()
+								Text(crypto.priceChangePercentage24h > 0 ? "+" : "")
+								Text("\(String(format: "%.2f", crypto.priceChangePercentage24h)) %")
+									.font(.caption)
+							
+							}
+							Text(String(format: "$%.2f", crypto.currentPrice))
+								.font(.title3)
+						}
+						.listItemTint(crypto.priceChangePercentage24h > 0 ? .green : .red	)
+					}
+				}
+			}
+			.navigationTitle("C4F")
+		}
+		.onAppear {
+			Task {
+				do {
+					let allCrypto = try await CryptoApi.fetchCryptoCurrency()
+					cryptos = allCrypto
+				} catch {
+					throw error
+				}
+			}
+		}
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
